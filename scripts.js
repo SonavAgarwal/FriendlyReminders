86867a8db57eab7f4e6a6b3a1384a1d76dbf5d59
Wyatt Cowley <wycowley@gmail.com>
2020-06-13 15:06:40 -0700

    getting rid of alerts
    woohoo it works sort of

--- scripts.js
+++ scripts.js
@@ -1,19 +1,17 @@
 const supported = ('contacts' in navigator && 'ContactsManager' in window);
-console.log("does it work? "+supported);
+
 
 async function openUpContact(){
     console.log("test");
-    window.alert("supported"+supported);
+    window.alert(supported);
     if(supported){
         const props = ['name'];
         const opts = {multiple: true};
-        window.alert("it is, i repeat it is supported");
         try {
         const contacts = await navigator.contacts.select(props, opts);
         handleResults(contacts);
         } catch (ex) {
         // Handle any errors here.
-            window.alert("Please im so sad");
             window.alert(ex);
         }
     }
@@ -29,27 +27,5 @@ function handleResults(contacts) {
   
   function renderResults(contacts) {
     
-    contacts.forEach((contact) => {
-      const lines = [];
-      if (contact.name) lines.push(`<b>Name:</b> ${contact.name.join(', ')}`);
-      if (contact.email) lines.push(`<b>E-mail:</b> ${contact.email.join(', ')}`);
-      if (contact.tel) lines.push(`<b>Telephone:</b> ${contact.tel.join(', ')}`);
-      if (contact.address) {
-        contact.address.forEach((address) => {
-          lines.push(`<b>Address:</b> ${JSON.stringify(address)}`);
-        });
-      }
-      if (contact.icon) {
-        contact.icon.forEach((icon) => {
-          const imgURL = URL.createObjectURL(icon);
-          lines.push(`<b>Icon:</b> <img src="${imgURL}">`);
-        });      
-      }
-      lines.push(`<b>Raw:</b> <code>${JSON.stringify(contact)}</code>`);
-      const li = document.createElement('li');
-      li.innerHTML = lines.join('<br>');
-      ulResults.appendChild(li);
-    });
-    const strContacts = JSON.stringify(contacts, null, 2);
-    console.log(strContacts);
+    window.alert(contacts);
   }
